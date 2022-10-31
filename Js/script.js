1cad6bdc04c16f0e51b7ee90ef098ed4e5c6862b
Hikmot <69507776+Hikmot-O@users.noreply.github.com>
2022-10-31 12:19:43 +0100

    removed commented codes

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,7 @@
 "use strict";
+
 let rating = "";
 
-const ratings = document.querySelector(".ratings");
 const ratingNumber = document.querySelectorAll(".rating-number");
 const ratingCard = document.querySelector(".rating-card");
 const ratingValue = document.querySelector(".rating-value");
@@ -9,34 +9,15 @@ const thankYouCard = document.querySelector(".thankyou-card");
 const btn = document.querySelector(".btn");
 const btnText = document.querySelector(".btnText");
 
-// console.log(ratingNumber.length);
-
-// ratings.addEventListener("click", (e) => {
-//   const element = e.target;
-
-//   // Array.from(ratingNumber).map(
-//   //   (el) => (el.style.background = "rgba(149, 158, 172, 0.15)")
-//   // );
-//   console.log(element.className === "rating-number circle");
-//   if (element.className === "rating-number circle") {
-//     //Update UI
-//     // element.style.background = "hsl(25, 97%, 53%)";
-//     // element.style.color = "hsl(0, 0%, 100%)";
-
-//     element.classList.add("rating-active");
-
-//     //Update state
-//     rating = element.textContent;
-//   } else {
-//     return;
-//   }
-
-  // Array.from(ratingNumber).map((el) =>
-  //   el.addEventListener("click", function (e) {
-  //     el.classList.toggle("rating-active");
-  //   })
-  // );
-// });
+for(let i=0; i<ratingNumber.length; i++){
+  ratingNumber[i].addEventListener('click', ()=>{
+    for(let j=0; j<ratingNumber.length; j++){
+      ratingNumber[j].classList.remove('rating-active');
+    }
+    ratingNumber[i].classList.add('rating-active');
+    rating = ratingNumber[i].textContent;
+  });
+}
 
 const submitHandler = (e) => {
   e.preventDefault();
@@ -49,18 +30,3 @@ const submitHandler = (e) => {
 
 btn.addEventListener("click", submitHandler);
 btnText.addEventListener("click", submitHandler);
-
-// for(let i=0; i<ratingNumber.length; i++){
-//   console.log(ratingNumber[i].className.includes('circle'));
-// }
-//element.classList.add("quote");
-for(let i=0; i<ratingNumber.length; i++){
-  // console.log(ratingNumber[i].className.includes('circle'));
-  ratingNumber[i].addEventListener('click', ()=>{
-    for(let j=0; j<ratingNumber.length; j++){
-      ratingNumber[j].classList.remove('rating-active');
-    }
-    ratingNumber[i].classList.add('rating-active');
-    rating = ratingNumber[i].textContent;
-  })
-}
